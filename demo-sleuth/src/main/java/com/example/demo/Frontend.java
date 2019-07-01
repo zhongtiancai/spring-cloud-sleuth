e71cf9bc85f464baa0501f300cb6fabf4dcd5d90
zhongtiancai-PC\zhongtiancai <zhongtiancai@live.com>
2019-07-01 22:06:05 +0800

    no message

---
+++ demo-sleuth/src/main/java/com/example/demo/Frontend.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableAutoConfiguration
+@RestController
+public class Frontend {
+  private static Logger logger = LoggerFactory.getLogger(Frontend.class);
+  @Autowired
+  RestTemplate restTemplate;
+
+  String backendBaseUrl = System.getProperty("spring.example.backendBaseUrl", "http://localhost:9000");
+
+  @RequestMapping("/")
+  public String callBackend() {
+    logger.info("callbackend");
+    return restTemplate.getForObject(backendBaseUrl + "/api", String.class);
+  }
+
+  @Bean
+  RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Frontend.class,
+        "--spring.application.name=frontend",
+        "--server.port=8081"
+    );
+  }
+}
---
+++ demo-zipkin-mq/src/main/java/com/example/demo/Backend.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class Backend {
+  private static Logger logger = LoggerFactory.getLogger(Backend.class);
+  @RequestMapping("/api") public String printDate() {
+
+    logger.info("i am backend");
+    return new Date().toString();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Backend.class,
+        "--spring.application.name=backendMQ",
+        "--server.port=9022"
+    );
+  }
+}
